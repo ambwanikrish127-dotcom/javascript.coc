3c1e427420258e80833ba769e4f0cf994005498a
krish <ambwanikrish127@gmail.com>
2026-06-06 12:50:37 +0000

    data

---
+++ basics/datatypes.js
@@ -0,0 +1,17 @@
+"use strict " // treat all the js as a new version
+let age = 18
+let isLoggedIn = false
+let state = null
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+console.log(typeof "krish");
+console.log(typeof undefined);
+console.log(typeof null);
+// null ek object hai but undefined ek type hai
---
+++ basics/variables.js
@@ -0,0 +1,13 @@
+const accountId = 14433
+// accountId = 2 //  not allowed
+let accountemail = "krish@123"
+var pass = "123"
+city = "jaipur"
+let accountstate;
+
+accountemail = "kk@123"
+pass = "3"
+city = "bombay"
+console.log(accountId);
+console.table([accountId, accountemail, pass,city , accountstate])
+
