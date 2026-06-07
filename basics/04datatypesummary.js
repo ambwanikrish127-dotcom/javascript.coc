c4e9becff68f7001bb69b7369bcf8827cda41fd3
krish <ambwanikrish127@gmail.com>
2026-06-07 05:52:15 +0000

    Added datatypes summary examples

---
+++ basics/04datatypesummary.js
@@ -0,0 +1,37 @@
+// 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+
+let userEmail
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId)
+// Reference (Non Primitive)
+
+// Array, Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"];
+
+let myObj = {
+    name: "hitesh",
+    age: 22,
+}
+
+const myFunction = function() {
+    console.log("Hello world");
+}
+
+// console.log(typeof score);       // number
+// console.log(typeof isLoggedIn);  // boolean
+// console.log(typeof outsideTemp); // object
+// console.log(typeof userEmail);   // undefined
+// console.log(typeof id);          // symbol
+// console.log(typeof heroes);      // object
+// console.log(typeof myObj);       // object
+// console.log(typeof myFunction);  // function
